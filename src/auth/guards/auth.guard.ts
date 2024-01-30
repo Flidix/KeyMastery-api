441e50e106e639114e04d5c50d3321fa1e25ec85
Flidix <marko.lazarchuk@gmail.com>
2024-01-30 08:18:56 +0200

    initial commint

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,25 +1,53 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 
+import { JwtPayload, decode } from 'jsonwebtoken';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const req = context.switchToHttp().getRequest();
-    try {
-      const authHeader = req.headers.authorization;
-      const bearer = authHeader.split(' ')[0];
-      const token = authHeader.split(' ')[1];
-      if (bearer !== 'Bearer' || !token) {
-        throw new UnauthorizedException({ message: 'користувач не авторизований' });
-      }
-      const user = this.jwtService.verify(token);
-      req.user = user;
-      return true;
-    } catch (e) {
-      throw new UnauthorizedException('користувач не авторизований');
+    const authHeader = req.headers.authorization;
+
+    console.log('Auth Header:', authHeader);
+
+    if (!authHeader) {
+      console.log('Authorization header missing');
+      throw new UnauthorizedException({ message: 'unauthorized' });
     }
+
+    const [bearer, token] = authHeader.split(' ');
+
+    console.log('Bearer:', bearer);
+    console.log('Token:', token);
+
+    if (bearer !== 'Bearer' || !token) {
+      console.log('Invalid token format');
+      throw new UnauthorizedException({ message: 'unauthorized' });
+    }
+
+    const user = decode(token) as JwtPayload;
+
+    console.log('Decoded User:', user);
+
+    if (!user || typeof user !== 'object') {
+      console.log('Token could not be decoded');
+      throw new UnauthorizedException({ message: 'unauthorized' });
+    }
+
+    const expiration = user.exp;
+    const currentTime = Math.floor(Date.now() / 1000);
+
+    console.log('Expiration:', expiration);
+    console.log('Current Time:', currentTime);
+
+    if (expiration && currentTime > expiration) {
+      console.log('Token has expired');
+      throw new UnauthorizedException({ message: 'Token has expired' });
+    }
+
+    req.ctx = user;
+
+    return true;
   }
 }
